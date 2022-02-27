734a884ac30d1ef7780828c5857e4c2f3ff8d861
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-02-27 09:32:52 -0300

    Criação do componente cards
    Com props para serem usados depois.

---
+++ projetocards/src/components/Cards/Index.js
@@ -0,0 +1,32 @@
+import styles from "./Index.module.css";
+
+const Cards = (props) => {
+  return (
+    /* Container onde irão ficar todos os cartões */
+    <div className={styles.cardWrapper}>
+      {/* Container especifico dos cartões e tudo dentro deles */}
+      <div className={styles.cardContainer}>
+        {/* Botão de menu */}
+        <div className={styles.menuBtn}>...</div>
+        {/* Foto de perfil */}
+        <img src={props.img} alt="" className={styles.profileImg} />
+        {/* Container com as informações */}
+        <div className={styles.infoContainer}>
+          {/* Nome */}
+          <h1 className={styles.nome}>{props.nome}</h1>
+          {/* Cargo */}
+
+          <h3 className={styles.cargo}>{props.cargo}</h3>
+          {/* Numero de Telefone */}
+
+          <h1 className={styles.numeroTelefone}>{props.numeroTelefone}</h1>
+          {/* Email */}
+
+          <h1 className={styles.email}>{props.email}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
