863cdb4e725491ed644f639e9b9ead99ca6d4c18
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-02-27 10:05:13 -0300

    CSS inicial dos cartões
    Começando CSS

--- projetocards/src/components/Info/Info.js
+++ projetocards/src/components/Info/Info.js
@@ -11,6 +11,30 @@ const Info = () => {
         numeroTelefone="(61)9-98454560"
         email="nairon.luiz@gmail.com"
       />
+
+      <Cards
+        img="https://media.discordapp.net/attachments/935611068934418472/944197874185547786/20220218_084447.jpg?width=507&height=676"
+        nome="Nairon Luiz"
+        cargo="Frontend Developer"
+        numeroTelefone="(61)9-98454560"
+        email="nairon.luiz@gmail.com"
+      />
+
+      <Cards
+        img="https://media.discordapp.net/attachments/935611068934418472/944197874185547786/20220218_084447.jpg?width=507&height=676"
+        nome="Nairon Luiz"
+        cargo="Frontend Developer"
+        numeroTelefone="(61)9-98454560"
+        email="nairon.luiz@gmail.com"
+      />
+
+      <Cards
+        img="https://media.discordapp.net/attachments/935611068934418472/944197874185547786/20220218_084447.jpg?width=507&height=676"
+        nome="Nairon Luiz"
+        cargo="Frontend Developer"
+        numeroTelefone="(61)9-98454560"
+        email="nairon.luiz@gmail.com"
+      />
     </div>
   );
 };
