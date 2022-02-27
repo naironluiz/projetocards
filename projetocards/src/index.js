c5eaaf8b267f22895a0e223ecbc2ffe08139e142
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-02-27 09:35:06 -0300

    Teste de renderização do componente cards

--- projetocards/src/index.js
+++ projetocards/src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+/* Teste de renderização */
+import Cards from "./components/Cards/Index";
+
 const root = document.getElementById("root");
+
+ReactDOM.render(<Cards />, root);
