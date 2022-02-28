c79d43dfdf02deaebc427b67d2183bd0907eab31
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-02-28 11:24:16 -0300

    Icones adicionados
    Icones adicionados e formatados

--- projetocards/src/components/Info/Info.js
+++ projetocards/src/components/Info/Info.js
@@ -17,7 +17,7 @@ const Info = () => {
         nome="Nairon Luiz"
         cargo="Frontend Developer"
         numeroTelefone="(61)9-98454560"
-        email="nairon.luiz@gmail.com"
+        email="nairon.luiz555555555@gmail.com"
       />
 
       <Cards
