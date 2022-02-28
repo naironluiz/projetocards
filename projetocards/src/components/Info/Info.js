8300ebf5c6641906164fa402ba73cb1852b021dd
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-02-28 09:19:03 -0300

    Update Info.js
    Começando a colocar outras imagens e props

--- projetocards/src/components/Info/Info.js
+++ projetocards/src/components/Info/Info.js
@@ -5,7 +5,7 @@ const Info = () => {
   return (
     <div>
       <Cards
-        img="https://media.discordapp.net/attachments/935611068934418472/944197874185547786/20220218_084447.jpg?width=507&height=676"
+        img="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
         nome="Nairon Luiz"
         cargo="Frontend Developer"
         numeroTelefone="(61)9-98454560"
