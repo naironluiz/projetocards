e36313661582949f16bf704618fa0b54ef4d18a0
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-02-28 09:27:07 -0300

    Create img
    Criando uma pasta de repositorio de imagens

--- projetocards/src/components/Info/Info.js
+++ projetocards/src/components/Info/Info.js
@@ -13,7 +13,7 @@ const Info = () => {
       />
 
       <Cards
-        img="https://media.discordapp.net/attachments/935611068934418472/944197874185547786/20220218_084447.jpg?width=507&height=676"
+        img="https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
         nome="Nairon Luiz"
         cargo="Frontend Developer"
         numeroTelefone="(61)9-98454560"
