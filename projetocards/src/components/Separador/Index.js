ca1ecb6d3e07bd250ccadf74d8bcb8bed17f7432
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-01 10:09:40 -0300

    Update Index.js
    Estilizando o separador

--- projetocards/src/components/Separador/Index.js
+++ projetocards/src/components/Separador/Index.js
@@ -1,6 +1,13 @@
 import styles from "./index.module.css";
 
 const separador = (props) => {
-  return <p className={styles.separador}>{props.separador}</p>;
+  return (
+    <>
+      <p className={styles.separador}>
+        {props.separador}
+        <p className={styles.separador2}>{props.separador}</p>
+      </p>
+    </>
+  );
 };
 export default separador;
