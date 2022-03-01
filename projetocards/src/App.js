a2b13209b25d335dfd9c10dfb797bc0793eebf5c
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-01 09:43:39 -0300

    Update App.js
    Testando nova configuração de css pra container wrapper

--- projetocards/src/App.js
+++ projetocards/src/App.js
@@ -1,6 +1,7 @@
 import Info from "./components/Info/Info";
 import CardDiv from "./components/CardDiv/Index";
 import Separador from "./components/Separador/Index.js";
+import Backend from "./components/Info/Backend";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Separador separador="Frontend" />
       <CardDiv />
       <Separador separador="Backend" />
-      <CardDiv />
+      <Backend />
       <Separador separador="Design UX/UI" />
       <CardDiv />
     </div>
