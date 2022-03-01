7111a35e20ec5a1aba00d13d731a36834a7ef4b8
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-01 11:09:53 -0300

    Update index.js
    Adicionando comentários

--- projetocards/src/index.js
+++ projetocards/src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-/* Teste de renderização */
+/* Importar o app */
 import App from "./App";
 
+/* Capturando o root do html em uma variavel */
 const root = document.getElementById("root");
 
+/* Renderizando o projeto */
 ReactDOM.render(<App />, root);
