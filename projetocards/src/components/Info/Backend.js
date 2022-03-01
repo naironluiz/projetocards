af43218be57f7b3b280e7b82780dfc10ef7be24d
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-01 10:04:05 -0300

    Update de cards e dados de cards
    Mais cards, fotos e etc

--- projetocards/src/components/Info/Backend.js
+++ projetocards/src/components/Info/Backend.js
@@ -6,19 +6,19 @@ const Backend = () => {
   return (
     <div className={styles.cardDiv}>
       <Cards
-        img="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        img="https://images.unsplash.com/photo-1549545931-59bf067af9ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80"
         nome="Rebecca Luiz Silva"
         cargo="Backend Developer"
         numeroTelefone="(61)9-955565654"
-        email="reb.luiz@gmail.com"
+        email="reb.luiz112564@gmail.com"
       />
 
       <Cards
-        img="https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
-        nome="João Carlos"
+        img="https://images.unsplash.com/photo-1599577180589-0a0b958016b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
+        nome="Gercina Gomes"
         cargo="Backend Developer"
         numeroTelefone="(11)9-12345678"
-        email="jc.553@hotmail.com"
+        email="cina.553@hotmail.com"
       />
 
       <Cards
@@ -26,7 +26,7 @@ const Backend = () => {
         nome="Thancredo Águas"
         cargo="Frontend Developer"
         numeroTelefone="(61)9-12345678"
-        email="this.is.thancred@gmail.com"
+        email="this.is.thancred3@gmail.com"
       />
 
       <Cards
@@ -34,7 +34,7 @@ const Backend = () => {
         nome="Antonio Lisboa"
         cargo="Frontend Developer"
         numeroTelefone="(61)9-12345678"
-        email="antonio.lisboa.gomes@gmail.com"
+        email="antonio.lisboa.gomes555@gmail.com"
       />
     </div>
   );
--- projetocards/src/components/Info/Frontend.js
+++ projetocards/src/components/Info/Frontend.js
@@ -9,7 +9,7 @@ const Frontend = () => {
         nome="Rebecca Luiz Silva"
         cargo="Frontend Developer"
         numeroTelefone="(61)9-955565654"
-        email="reb.luiz@gmail.com"
+        email="reb.luizzz@gmail.com"
       />
 
       <Cards
@@ -25,7 +25,7 @@ const Frontend = () => {
         nome="Thancredo Águas"
         cargo="Frontend Developer"
         numeroTelefone="(61)9-12345678"
-        email="this.is.thancred@gmail.com"
+        email="this.is.thancred33@gmail.com"
       />
 
       <Cards
@@ -33,7 +33,23 @@ const Frontend = () => {
         nome="Antonio Lisboa"
         cargo="Frontend Developer"
         numeroTelefone="(61)9-12345678"
-        email="antonio.lisboa.gomes@gmail.com"
+        email="antonio.lisboa.gomes11@gmail.com"
+      />
+
+      <Cards
+        img="https://images.unsplash.com/photo-1535295972055-1c762f4483e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        nome="Ivana Cajina"
+        cargo="Frontend Developer"
+        numeroTelefone="(61)9-12345678"
+        email="ivana.cajina3@gmail.com"
+      />
+
+      <Cards
+        img="https://images.unsplash.com/flagged/photo-1573807813964-b8eb2626a50e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+        nome="Arthur  Soares Souza"
+        cargo="Frontend Developer"
+        numeroTelefone="(61)9-12345678"
+        email="arthur.soares.souza66@gmail.com"
       />
     </div>
   );
