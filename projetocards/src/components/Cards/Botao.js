ecc7ea84b698dfa13c9e13a98a210d1bd90536fd
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-01 11:00:26 -0300

    Update Botao.js
    Adicionando comentários

--- projetocards/src/components/Cards/Botao.js
+++ projetocards/src/components/Cards/Botao.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import styles from "./Index.module.css";
 
 function AtivarBotao() {
+  /* setar como falso */
   const [isActive, setActive] = useState(false);
 
   return (
+    /* Uma vez que essa div seja clicado, será togglado como ativo e vice e versa.*/
     <div
       className={`${styles.menuBtn} ${isActive ? `${styles.active}` : ""}`}
       onClick={() => setActive(!isActive)}
     >
       ...
+      {/* O toggle serve para ativar o css dessa lista */}
       <ul className={styles.listaOpc}>
         <li>Alterar Foto</li>
         <li>Mudar Nome</li>
