ffa069d8541567d06fa39b3b51ab07315857c0e7
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-04 10:16:19 -0300

    Update Index.js
    Fragmentação no separador

--- projetocards/src/components/Separador/Index.js
+++ projetocards/src/components/Separador/Index.js
@@ -1,6 +1,10 @@
 import styles from "./index.module.css";
 
 const separador = (props) => {
-  return <p className={styles.separador}>{props.separador}</p>;
+  return (
+    <>
+      <p className={styles.separador}>{props.separador}</p>
+    </>
+  );
 };
 export default separador;
