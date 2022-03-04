98c5dfa326d6179e0ee79f4090050a691157e5d9
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-04 10:19:30 -0300

    Update Index.js
    Atualizando props no index.

--- projetocards/src/components/Cards/Index.js
+++ projetocards/src/components/Cards/Index.js
@@ -2,7 +2,7 @@ import styles from "./Index.module.css";
 import AtivarBotao from "./Botao";
 
 import React from "react";
-const Cards = (props) => {
+const Cards = ({ img, nome, cargo, numeroTelefone, email }) => {
   return (
     /* Container onde irão ficar todos os cartões */
     <div className={styles.cardWrapper}>
@@ -13,20 +13,20 @@ const Cards = (props) => {
         {/* Lista de botões do menu */}
 
         {/* Foto de perfil */}
-        <img src={props.img} alt="" className={styles.profileImg} />
+        <img src={img} alt="" className={styles.profileImg} />
         {/* Container com as informações */}
         <div className={styles.infoContainer}>
           {/* Nome */}
-          <h1 className={styles.nome}>{props.nome}</h1>
+          <h1 className={styles.nome}>{nome}</h1>
           {/* Cargo */}
           <div className="teste"></div>
-          <h3 className={styles.cargo}>{props.cargo}</h3>
+          <h3 className={styles.cargo}>{cargo}</h3>
           {/* Numero de Telefone */}
 
-          <h1 className={styles.numeroTelefone}>{props.numeroTelefone}</h1>
+          <h1 className={styles.numeroTelefone}>{numeroTelefone}</h1>
           {/* Email */}
 
-          <h1 className={styles.email}>{props.email}</h1>
+          <h1 className={styles.email}>{email}</h1>
         </div>
       </div>
     </div>
